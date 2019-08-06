33b78ea56feac47579483f388e1225bb2ede3cc0
madamczyk <madamczyk@tbscg.com>
2019-08-06 09:46:51 +0200

    Update

--- java_app/attributes/default.rb
+++ java_app/attributes/default.rb
@@ -1,3 +1,7 @@
 node.default['java']['jdk_version'] = '8'
 node.default['java']['install_flavor'] = 'openjdk'
 node.default['java']['accept_license_agreement'] = true
+node.default['packages-cookbook'] = [
+ 'nodejs',
+ 'imagemagick'
+]
